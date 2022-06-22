65aeca4ef145c2d30ed30f2bee3826caf9de0fbc
Mihaja <raharison05@gmail.com>
2022-06-22 14:21:13 +0300

    Login selon le modele

--- app/Http/Controllers/logCotroller.php
+++ app/Http/Controllers/logCotroller.php
@@ -2,21 +2,27 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class logCotroller extends Controller
 {
     public function login(){
         return view('login');
     }
-    public function auth(Request $request){
+    public function auth(Request $request, User $user){
         //validation des requete de type mail
         $request-> validate([
             'email' => 'required|email',
             'password' => 'required'
 
         ]);
-        if (auth()->attempt($request->only('email','password'))){
+        Auth::login($user);
+        $credentials=$request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
             return redirect()->route('dashboard');
         }
         return redirect()->back()->withErrors('biby');
